d08470cf2b73d686f9f4d99b3d81f66e055da46a
Pradip Caulagi <caulagi@gmail.com>
2020-01-04 15:15:35 +0100

    Add data file and script to generate it
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

---
+++ data/data.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:57f7db435b25cc20ef468375c52e0dee3572c8dbeb4ebe41d08ec46163b05a2d
+size 7896852
---
+++ data/gen.py
@@ -0,0 +1,54 @@
+"""
+Script to generate sql load script for the quotation table.
+The dataset used is - https://www.kaggle.com/akmittal/quotes-dataset is base
+The quotes.json from the dataset should be in this directory
+
+    >>> python gen.py > data.sql
+"""
+import hashlib
+import json
+
+
+print(
+    """
+CREATE TABLE IF NOT EXISTS quotation(
+    id SERIAL PRIMARY KEY,
+    context TEXT,
+    sha256 TEXT unique,
+    author TEXT NULL,
+    created TIMESTAMP DEFAULT now(),
+    modified TIMESTAMP
+);
+
+CREATE FUNCTION update_modified_column() RETURNS TRIGGER AS $update_modified_column$
+BEGIN
+    NEW.modified = now();
+    RETURN NEW;
+END;
+$update_modified_column$ language 'plpgsql';
+
+CREATE TRIGGER update_modified_column BEFORE INSERT OR UPDATE ON quotation
+    FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+
+INSERT INTO quotation(context, sha256, author) VALUES""")
+
+with open("quotes.json") as f:
+    data = json.loads(f.read())
+
+seen = {}
+for row in data:
+    m = hashlib.md5()
+    m.update(row["Quote"].encode("utf-8"))
+    md5 = m.hexdigest()
+    if md5 not in seen:
+        quote = row['Quote'].replace("'", "’")
+        author = row['Author'].replace("'", "’")
+        out = (quote, md5, author)
+        print(f"{out},")
+        seen[md5] = 1
+
+quote = "Strategy, overdone; doing stuff, underdone"
+m = hashlib.md5()
+m.update(quote.encode("utf-8"))
+md5 = m.hexdigest()
+print(f"('{quote}', '{md5}', 'Carlo Ancelotti quoting Herb Keller, Quiet Leadership');")
